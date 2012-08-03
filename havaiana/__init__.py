d56fe90e24369a15ca60ba130dbeaa8d993f42ca
Felipe Lerena <felipelerena@gmail.com>
2012-08-03 07:51:07 -0300

    adding renderers

--- havaiana/__init__.py
+++ havaiana/__init__.py
@@ -3,7 +3,7 @@ from inspect import getmembers
 from ojota import Ojota
 
 
-def get_ojotines(package):
+def get_ojota_children(package):
     hijos = []
     items = getmembers(package)
     for item in items:
@@ -14,11 +14,31 @@ def get_ojotines(package):
             pass
     return hijos
 
-def run(package):
-    ojotines = get_ojotines(package)
+def default_renderer(field, item):
+    required = field in item.required_fields
+    value = getattr(item, field)
+    relation_data = item.relations.get(field)
+
+    if value is None or relation_data is None:
+        related = False
+    else:
+        related = "/%s/%s" % (relation_data[0].plural_name, value)
+
+    return (field, value, required, related)
+
+def render_field(field, item, renderers):
+    render = default_renderer
+    for renderer in renderers:
+        if renderer[0] == field:
+            render = renderer[1]
+
+    return render(field, item)
+
+def run(package, renderers=None):
+    classes = get_ojota_children(package)
     app = Flask(__name__)
     classes_map = {}
-    for item in ojotines:
+    for item in classes:
         classes_map[item[1].plural_name] = item
         #print item[1].all()
 
@@ -27,6 +47,8 @@ def run(package):
     def table(name, pk=None):
         item = classes_map[name]
         cls = item[1]
+        class_renderers = [renderer[1:] for renderer in renderers
+                           if renderer[0] == item[0]]
         if pk is None:
             return render_template('table.html', items=cls.all(),
                                    class_name=name)
@@ -35,14 +57,7 @@ def run(package):
             item = cls.get(**params)
             attrs = []
             for field in item.fields:
-                required = field in item.required_fields
-                value = getattr(item, field)
-                relation_data = item.relations.get(field)
-                if value is None or relation_data is None:
-                    related = False
-                else:
-                    related = "/%s/%s" % (relation_data[0].plural_name, value)
-                attrs.append((field, value, required, related))
+                attrs.append(render_field(field, item, class_renderers))
 
             return render_template('item.html', item=item, attrs=attrs,
                                    class_name=name)
--- havaiana/example.py
+++ havaiana/example.py
@@ -3,6 +3,18 @@ from ojota import current_data_code
 
 import msa.core.data as pkg
 
+def candidato_list(field, item):
+    required = field in item.required_fields
+    lista_cand = getattr(item, field)
+    value = ""
+    for element in lista_cand:
+        value += '<a href="/Candidatos/%s">%s</a> 1' % (element, element)
+
+    related = False
+
+    return (field, value, required, related)
+
 if __name__ == '__main__':
     current_data_code("EJ01")
-    run(pkg)
+    renderers = [('Lista', 'cod_candidatos', candidato_list)]
+    run(pkg, renderers)
