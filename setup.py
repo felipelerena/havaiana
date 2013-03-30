4e3ef8edc60faf012c843f76cbd2a3ab30de336d
Felipe Lerena <felipelerena@gmail.com>
2013-03-29 21:09:57 -0300

    updating version and documentation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='Havaiana',
-    version='2.0.0',
+    version='2.1.0',
     author='Felipe Lerena',
     author_email='felipelerena@gmail.com',
     packages=['havaiana'],
